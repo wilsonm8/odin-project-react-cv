475e0ef1753d76577ef5179e73caf6107b8c5e36
wilsonm8 <103072620@student.swin.edu.au>
2025-02-13 05:07:07 +1100

    added education fields and functionality

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,20 +1,34 @@
-function School() {
+import { useState } from "react";
+import InputField from "./InputField";
 
-};
+function Education({editable}) {
+    const [education, setEducation] = useState({ school: "", title: "", date: "" });
 
-function Title() {
-
-};
-function Date() {
-
-};
-
-function Education() {
+    const handleChange = (field) => (value) => {
+        setEducation((prevEducation) => ({ ...prevEducation, [field]: value }));
+    };
     return (
         <>
-            <School />
-            <Title />
-            <Date />
+            <h2>Experience:</h2>
+            <InputField
+                editable={editable}
+                onSubmit={handleChange("school")}
+                value={education.school}
+                label="School"
+            />
+            <InputField
+                editable={editable}
+                onSubmit={handleChange("title")}
+                value={education.title}
+                label="Title"
+            />
+            <InputField
+                editable={editable}
+                onSubmit={handleChange("date")}
+                value={education.date}
+                label="Date"
+                type="date"
+            />
         </>
     )
 };
