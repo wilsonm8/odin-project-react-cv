95264764d7b20a5ce4ef9f4adba19b9c816bd892
wilsonm8 <103072620@student.swin.edu.au>
2025-02-13 04:57:47 +1100

    added experience fields and functionality

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,31 +1,74 @@
-function Company() {
+import { useState } from "react";
 
+function InputField({ editable, onSubmit, value, label, type = "text" }) {
+    const [inputValue, setInputValue] = useState(value);
+    const handleChange = (event) => {
+        setInputValue(event.target.value);
+        onSubmit(event.target.value);
+    };
+
+    if (!editable) {
+        return (
+            <div className="formGroup">
+                <label className="formLabel">{label}: </label>
+                <span>{value}</span>
+            </div>
+        );
+
+    };
+    return (
+        <div className="formGroup">
+            <label className="formLabel">{label}: </label>
+            <input
+                type={type}
+                onChange={handleChange}
+                value={inputValue}>
+            </input>
+        </div>
+    );
 };
 
-function Position() {
+function Experience({ editable }) {
+    const [experience, setExperience] = useState({ company: "", position: "", responsibilities: "", startDate: "", endDate: "" });
 
-};
-
-function Responsibilities() {
-
-};
-
-function StartDate() {
- 
-};
-
-function EndDate() {
-
-};
-
-function Experience() {
+    const handleChange = (field) => (value) => {
+        setExperience((prevExperience) => ({ ...prevExperience, [field]: value }));
+    };
     return (
         <>
-            <Company />
-            <Position />
-            <Responsibilities />
-            <StartDate />
-            <EndDate />
+            <h2>Experience:</h2>
+            <InputField
+                editable={editable}
+                onSubmit={handleChange("company")}
+                value={experience.company}
+                label="Company"
+            />
+            <InputField
+                editable={editable}
+                onSubmit={handleChange("position")}
+                value={experience.position}
+                label="Position"
+            />
+            <InputField
+                editable={editable}
+                onSubmit={handleChange("responsibilities")}
+                value={experience.responsibilities}
+                label="Responsibilities"
+            />
+            <InputField
+                editable={editable}
+                onSubmit={handleChange("startDate")}
+                value={experience.startDate}
+                label="Start Date"
+                type="date"
+            />
+            <InputField
+                editable={editable}
+                onSubmit={handleChange("endDate")}
+                value={experience.endDate}
+                label="End Date"
+                type="date"
+            />
         </>
     );
 };
